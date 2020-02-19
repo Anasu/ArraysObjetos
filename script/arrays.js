6c4f370c5972763a060b700952e8b29a3b54c7b6
Ana <misushima@gmail.com>
2020-02-19 15:33:05 -0300

    Se agrega estructura base del proyecto

---
+++ script/arrays.js
@@ -0,0 +1,13 @@
+window.onload() = function 
+{
+    document.write('<h1>Estadisticas centro médico ñuñoa</h1>');
+
+    let radiologia = [];
+    let traumatilogía = [];
+    let dental = [];
+
+    //Mostrar por pantalla la cantidad de consultas por cada listado 
+    //Mostrar primera y ultima atencion de cada listado, desplegando
+    //nombre, rut y prevision separados por guión
+    
+}
