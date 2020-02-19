c55d873be87e727d06b52cf8f1f2ab2c1783e562
Ana <misushima@gmail.com>
2020-02-19 16:48:08 -0300

    Se agregan datos de radiologia

--- script/arrays.js
+++ script/arrays.js
@@ -2,9 +2,43 @@ window.onload() = function
 {
     document.write('<h1>Estadisticas centro médico ñuñoa</h1>');
 
-    let radiologia = [];
-    let traumatilogía = [];
-    let dental = [];
+    let radiologia = [
+        {
+            hora: "11:00",
+            especialista: "IGNACIO SCHULZ",
+            paciente: "FRANCISCA ROJAS",
+            rut: "9878782-1",
+            prevision: "FONASA"
+        },
+        {
+            hora: "11:30",
+            especialista: "FEDERICO SUBERCASEAUX",
+            paciente: "PAMELA ESTRADA",
+            rut: "15345241-3",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "15:00",
+            especialista: "FERNANDO WURTHZ",
+            paciente: "ARMANDO LUNA",
+            rut: "16445345-9",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "15:30",
+            especialista: "ANA MARIA GODOY",
+            paciente: "MANUEL GODOY",
+            rut: "17666419-0",
+            prevision: "FONASA"
+        },
+        {
+            hora: "16:00",
+            especialista: "PATRICIA SUAZO",
+            paciente: "RAMON ULLOA",
+            rut: "14989389-K",
+            prevision: "FONASA"
+        }
+    ];
 
     //Mostrar por pantalla la cantidad de consultas por cada listado 
     //Mostrar primera y ultima atencion de cada listado, desplegando
