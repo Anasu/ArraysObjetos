5e8df8fc5e14f90992b8b70ac155708aeda3e73b
Ana <misushima@gmail.com>
2020-02-19 16:48:49 -0300

    Se agregan datos de dental

--- script/arrays.js
+++ script/arrays.js
@@ -39,6 +39,57 @@ window.onload() = function
             prevision: "FONASA"
         }
     ];
+    let traumatilogía = [
+        {
+            hora: "8:00",
+            especialista: "MARIA PAZ ALTUZARRA",
+            paciente: "PAULA SANCHEZ",
+            rut: "15554774-5",
+            prevision: "FONASA"
+        },
+        {
+            hora: "10:00",
+            especialista: "RAUL ARAYA",
+            paciente: "ANGÉLICA NAVAS",
+            rut: "15444147-9",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "10:30",
+            especialista: "MARIA ARRIAGADA",
+            paciente: "ANA KLAPP",
+            rut: "17879423-9",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "11:00",
+            especialista: "ALEJANDRO BADILLA",
+            paciente: "FELIPE MARDONES",
+            rut: "1547423-6",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "11:30",
+            especialista: "CECILIA BUDNIK",
+            paciente: "DIEGO MARRE",
+            rut: "16554741-K",
+            prevision: "FONASA"
+        },
+        {
+            hora: "12:00",
+            especialista: "ARTURO CAVAGNARO",
+            paciente: "CECILIA MENDEZ",
+            rut: "9747535-8",
+            prevision: "ISAPRE"
+        },
+        {
+            hora: "12:30",
+            especialista: "ANDRES KANACRI",
+            paciente: "MARCIAL SUAZO",
+            rut: "11254785-5",
+            prevision: "ISAPRE"
+        }
+    ];
 
     //Mostrar por pantalla la cantidad de consultas por cada listado 
     //Mostrar primera y ultima atencion de cada listado, desplegando
