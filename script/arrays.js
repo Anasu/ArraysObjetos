667e180188ec8362fd90e91432e200e56b9dcc4f
Ana <misushima@gmail.com>
2020-02-19 22:17:26 -0300

    Se agrega primeras y ultimas atenciones por area

--- script/arrays.js
+++ script/arrays.js
@@ -12,5 +12,27 @@ window.onload = function ()
         " "+dental.length);
     //Mostrar primera y ultima atencion de cada listado, desplegando
     //nombre, rut y prevision separados por guión
+    document.write("<h2>Primeras y últimas atenciones</h2>");
+    document.write("<h3>Atenciones de Radiología</h3>")
+    document.write("<p>Primera atención: "+radiologia[0].paciente+
+        " - "+radiologia[0].rut+" - "+radiologia[0].prevision+"</p>");
+    document.write("<p>Última atención: "+radiologia[radiologia.length - 1].paciente+
+        " - "+radiologia[radiologia.length - 1].rut+" - "+
+        radiologia[radiologia.length - 1].prevision+"</p>"); 
+
+    document.write("<h3>Atenciones de Traumatología</h3>")
+    document.write("<p>Primera atención: "+traumatologia[0].paciente+
+        " - "+traumatologia[0].rut+" - "+traumatologia[0].prevision+"</p>");
+    document.write("<p>Última atención: "+traumatologia[traumatologia.length - 1].paciente+
+        " - "+traumatologia[traumatologia.length - 1].rut+" - "+
+        traumatologia[traumatologia.length - 1].prevision+"</p>");
+
+    document.write("<h3>Atenciones Dentales</h3>")
+    document.write("<p>Primera atención: "+dental[0].paciente+
+        " - "+dental[0].rut+" - "+dental[0].prevision+"</p>");
+    document.write("<p>Última atención: "+dental[dental.length - 1].paciente+
+        " - "+dental[dental.length - 1].rut+" - "+
+        dental[dental.length - 1].prevision+"</p>");    
+
     
 }
